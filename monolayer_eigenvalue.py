b5b1e1d939f3572d3424863f7bd96f6bdf98f70a
Chanchal Mandal <138012960+Chanchal-Mandal@users.noreply.github.com>
2023-10-27 12:14:32 +0530

    Update and rename hermitian.py to monolayer_eigenvalue.py
    Added the final code

--- monolayer_eigenvalue.py
+++ monolayer_eigenvalue.py
@@ -3,21 +3,23 @@ from numpy.linalg import eig
 import csv
 import matplotlib.pyplot as plt 
 
-n = 4 # no of hexagon
-m = 4# no of rows
+n = 31 # no of hexagon
+m = 31 # no of rows
 s = (2*n +1)*(m+1) # Total no of sites 
 t=1 # Hopping amplitude 
-
+print(s)
 H = np.zeros([s, s], dtype= int)
 
-# Code to find the eigen values of the hermitian system
+# Code to find the eigen values of this hamiltonian system
 for i in range(0, s-1) :      # Amplitude due to its preceeding and suceeding element except the boundary connection 
-      if i in range  (2*n,s,2*n+1) :
-         continue
+      if i in range (2*n+1,s,2*n+1) :
+          continue
       H[i, i+1] = 1 
+      if i in range (1,s,2*n+1) :
+         continue
       H[i+1, i] = 1 
       
-                  
+                
 for j in range(2*n+1, s) :   # Amplitude due to vertical connections sites at even place 
             if  j % 2 != 0 :
              H[j,j-(2*n+1)] = 1
@@ -26,42 +28,27 @@ for k in range(0, s-(2*n+1)) :  # Amplitude due to vertical connections sites at
             if k%2 == 0 :
                 H[k,k+(2*n+1)] = 1
 
+print(H)
 w,v = np.linalg.eig(H)
 z = np.array([w])
 #z.T
 z= np.sort(z)
 
-with open('Matrix.csv', 'w', encoding='UTF8', newline='') as f:
+# To write the CSV file for Hamilatonian Matrix and its eigen value 
+with open('Alayer.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(H)
-with open('eigenvalue.csv', 'w', encoding='UTF8', newline='') as f:
+with open('Aeigenvalue.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(z)
+
     
-"""rows,col = H.shape
-index_values = np.arange(1, rows + 1)
-H = np.insert(H, 0, index_values, axis=0)
-H = np.insert(H, 0, index_values, axis=1)"""
+f = plt.figure()
 
-w,v = np.linalg.eig(H)  
-print(H)
-print(w)
 
-# Code for Density of states :
-
-I = np.array([])
-E = np.array([])
-
-for l in np.arange (-4.0,4.0,0.01) :
-    I =np.append(I,l)
-    
-"""for m in range (1,s) :
-    x = (w[m])
-    E= np.append(E,x)"""
-    
-plt.hist(w,bins=I)
-plt.xlabel('Energy')
+plt.hist(w,bins=100)
+plt.xlabel('Energy Eigenvalue')
 plt.ylabel('DOS')
-plt.title("Density Of states")
+plt.title("Density Of states for Monolayer Graphene")
 plt.show()
 
